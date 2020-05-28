59833c3c687cc79b9d8989611c3ced8a9980f82d
Benita Diop <45861503+BenitaDiop@users.noreply.github.com>
2020-05-28 11:26:31 -0400

    Update classification.py

--- scripts/PySpark_Scripts/classification.py
+++ scripts/PySpark_Scripts/classification.py
@@ -27,3 +27,33 @@ text_df.orderBy(rand()).show(10,False)
 text_df.groupBy('Label').agg({'Length':'mean'}).show()
 
 
+
+
+
+
+# Data Cleaning
+tokenization=Tokenizer(inputCol='Review',outputCol='tokens')
+tokenized_df=tokenization.transform(text_df)
+
+tokenized_df.show()
+
+stopword_removal=StopWordsRemover(inputCol='tokens',outputCol='refined_tokens')
+refined_text_df=stopword_removal.transform(tokenized_df)
+
+refined_text_df.show()
+
+
+
+from pyspark.sql.functions import udf
+from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import *
+
+len_udf = udf(lambda s: len(s), IntegerType())
+
+refined_text_df = refined_text_df.withColumn("token_count", len_udf(col('refined_tokens')))
+
+refined_text_df.orderBy(rand()).show(10)
+count_vec=CountVectorizer(inputCol='refined_tokens',outputCol='features')
+cv_text_df=count_vec.fit(refined_text_df).transform(refined_text_df)
+cv_text_df.select(['refined_tokens','token_count','features','Label']).show(10)
+
