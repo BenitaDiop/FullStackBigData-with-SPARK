622be2f0f2d15756cda2b9a59d8b2f1eb19f11fe
Benita Diop <45861503+BenitaDiop@users.noreply.github.com>
2020-05-28 11:25:57 -0400

    Update classification.py

--- scripts/PySpark_Scripts/classification.py
+++ scripts/PySpark_Scripts/classification.py
@@ -1,1 +1,18 @@
 
+text_df=spark.read.csv('reviews.csv',inferSchema=True,header=True,sep=',')
+text_df.printSchema()
+text_df.count()
+
+from pyspark.sql.functions import rand 
+text_df.orderBy(rand()).show(10,False)
+text_df=text_df.filter(((text_df.Sentiment =='1') | (text_df.Sentiment =='0')))
+
+text_df.count()
+text_df.groupBy('Sentiment').count().show()
+
+
+text_df.printSchema()
+text_df = text_df.withColumn("Label", text_df.Sentiment.cast('float')).drop('Sentiment')
+text_df.orderBy(rand()).show(10,False)
+text_df.groupBy('label').count().show()
+
